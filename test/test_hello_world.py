70285304b3ce3442f6d46b2922643c16b3e1467d
Gérard TOKO <gerard.toko@dataleon.ai>
2026-05-19 12:05:55 +0200

    Update test_hello_world.py

--- test/test_hello_world.py
+++ test/test_hello_world.py
@@ -8,3 +8,21 @@ def test_hello_world():
     response = client.get('/')
     assert response.status_code == 200
     assert response.data.decode() == 'Hello, World!'
+
+def test_hello_world_2():
+    client = app.test_client()
+    response = client.post('/')
+    assert response.status_code == 405
+
+def test_blog_articles():
+    client = app.test_client()
+    response = client.post('/blogs',
+        json={
+            "title": "Test Article",
+            "content": "This is a test."
+        },
+        headers={"X-API-Key": "api-key-123"}
+    )
+    assert response.status_code == 201
+    data = response.get_json()
+    assert data['title'] == "Test Article"
